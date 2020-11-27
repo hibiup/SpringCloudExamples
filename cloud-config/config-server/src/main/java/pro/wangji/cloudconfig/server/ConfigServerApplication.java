4412cdd74e9346fae234ba21fb2601a6e86baa4c
Ji Wang <jeffwji@gmail.com>
2020-11-26 22:33:29 -0500

    Add cloud configuration

--- cloud-config/config-server/src/main/java/pro/wangji/cloudconfig/server/ConfigServerApplication.java
+++ cloud-config/config-server/src/main/java/pro/wangji/cloudconfig/server/ConfigServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer      // 启用 config server
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
