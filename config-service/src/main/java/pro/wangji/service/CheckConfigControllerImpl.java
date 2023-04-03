218493144e97f82d2d88c54d812e67e5217dcc26
Ji Wang <jeffwji@gmail.com>
2023-04-03 13:21:15 -0400

    Update common pattern

--- config-service/src/main/java/pro/wangji/service/CheckConfigControllerImpl.java
+++ config-service/src/main/java/pro/wangji/service/CheckConfigControllerImpl.java
@@ -3,8 +3,6 @@ package pro.wangji.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
