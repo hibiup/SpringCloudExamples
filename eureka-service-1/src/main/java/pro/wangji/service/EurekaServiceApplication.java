809721adf72f77bb0ea7d1fa0434a51da8a4550e
Ji Wang <jeffwji@gmail.com>
2023-04-02 22:22:22 -0400

    Add one service

--- eureka-service-1/src/main/java/pro/wangji/service/EurekaServiceApplication.java
+++ eureka-service-1/src/main/java/pro/wangji/service/EurekaServiceApplication.java
@@ -1,15 +1,15 @@
 package pro.wangji.service;
 
 //import brave.sampler.Sampler;
-import brave.sampler.Sampler;
+//import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,16 +21,16 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Eureka Service 是注册到 Eureka 上的服务，可以启动多个，随机分配端口。
+ * Eureka Service 1 是注册到 Eureka 上的服务，可以启动多个，随机分配端口。
  *
  * Eureka Service 的 /hello 服务被 Service Ribbon 以负载均衡的方式代理。
  */
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
-@EnableFeignClients
-@RestController
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+//@EnableFeignClients
+//@RestController
 public class EurekaServiceApplication {
     private static final Logger logger = Logger.getLogger(EurekaServiceApplication.class.getName());
 
@@ -54,51 +54,51 @@ public class EurekaServiceApplication {
      *
      * Sampler 是调用跟踪记录采样器。Spring Sleuth 采用低度侵入性的方案来跟踪调用过程，需要插入这个采样器才能工作。
      */
-    @Bean
-    public Sampler defaultSampler() {
+    //@Bean
+    /*public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
-    }
+    }*/
 
     /**
      * 定义一个 feign 接口调用 service-hello：
      */
-    @FeignClient(value = "service-backend", fallback = ServiceBackendDelegateFallback.class)
+    /*@FeignClient(value = "service-backend", fallback = ServiceBackendDelegateFallback.class)
     interface ServiceBackendDelegate {
         @RequestMapping(value = "/deep_hello",method = RequestMethod.GET)
         String sayHiFromFrontend(@RequestParam(value = "name") String name);
-    }
+    }*/
 
     /**
      * 当 ServiceBackendDelegate 失败时断路器 Feign 将访问该类。
      */
-    @Component
+    /*@Component
     class ServiceBackendDelegateFallback implements ServiceBackendDelegate {
         @Override
         public String sayHiFromFrontend(String name) {
             return "sorry "+name;
         }
-    }
+    }*/
 
-    @Autowired
-    ServiceBackendDelegate serviceBackendDelegate;
+    /*@Autowired
+    ServiceBackendDelegate serviceBackendDelegate;*/
 
     /**
      * Frontend 服务入口。调用  backend，会产生调用跟踪日志
      */
-    @RequestMapping("/bk")
+    /*@RequestMapping("/bk")
     public String callBackend(){
         logger.log(Level.INFO, "Generate calling trace for backend service  ");
         return serviceBackendDelegate.sayHiFromFrontend( "backend" );
-    }
+    }*/
 
     /**
      * 本地服务，不会产生调用跟踪日志
      */
-    @RequestMapping("/hello")
+    /*@RequestMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "jason") String name) {
         logger.log(Level.INFO, "Not generate calling trace");
         return "hi " + name + " ,i am from port:" + port;
-    }
+    }*/
 
 }
 
---
+++ eureka-service-1/src/main/java/pro/wangji/service/MyController.java
@@ -0,0 +1,32 @@
+package pro.wangji.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @Autowired
+    private Environment env;
+
+    @Value("${key1}")
+    private String key1;
+
+    @GetMapping(
+            value = "/key1",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getKey1() {
+        return String.format("The value of %s is %s...\n", "key1", key1);
+    }
+
+    @GetMapping(
+            value = "/{key_name}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getKeyValue(@PathVariable("key_name") String keyName) {
+        return String.format("The value of %s is %s...\n", keyName, env.getProperty(keyName));
+    }
+}
